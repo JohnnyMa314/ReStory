71fa334eb3c764b70fbb6a47e1e26ec952d4a37f
Johnny Ma <jlm10003@log-1.nyu.cluster>
2021-10-27 21:43:57 -0400

    initial commit

---
+++ ROC_prep.py
@@ -0,0 +1,8 @@
+### ReStory: Reordering Stories to Change Meaning
+### By Johnny Ma, He He, and Snigdha Chaturvedi
+
+def main():
+	home_dir = '/scratch/jlm10003/ReStory/data'
+
+if __name__ == '__main__':
+	main()
